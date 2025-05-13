e4d7b2c5afea928ce7c5f7d9f4e36b13d242d97c
aalopezb <a.alexander.l.b@gmail.com>
2025-05-12 21:22:46 -0500

    feat: :memo: Changes in dockerfile

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)  
+
+@app.route("/")
+def hora():
+    return {  
+        "hora_actual": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+
+if __name__ == "__main__":  
+    app.run(debug=True)
