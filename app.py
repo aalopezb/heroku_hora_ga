35e030d98e51b59107ce8c00fa5104979f8a5efa
aalopezb <a.alexander.l.b@gmail.com>
2025-05-12 21:54:17 -0500

    feat: :memo: Change in app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import os
 from flask import Flask
 from datetime import datetime
 
-app = Flask(__name__)  
+app = Flask(__name__)
 
 @app.route("/")
 def hora():
@@ -9,5 +10,6 @@ def hora():
         "hora_actual": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
 
-if __name__ == "__main__":  
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port, debug=True)
